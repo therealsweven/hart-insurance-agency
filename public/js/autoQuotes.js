e843c030bdb9ed1a6bda6b896ac4b66765dad392
therealsweven <robssimpson11@gmail.com>
2023-04-16 02:54:10 -0600

    Added most forms, need to add forms js to make functional

--- public/js/autoQuotes.js
+++ public/js/autoQuotes.js
@@ -1,4 +1,22 @@
-$("#autoQuoteImageContainer").on("click", function (event) {
+const quoteModal = $("#quoteModal");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close2")[0];
+
+$("#quoteImageContainer").on("click", function (event) {
   event.stopPropagation();
   console.log(event.target.parentElement.id);
+  quoteModal.show();
 });
+
+//When the user clicks on <span> (x), close the modal
+span2.onclick = function () {
+  quoteModal.hide();
+};
+
+//When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == quoteModal) {
+    quoteModal.hide();
+  }
+};
---
+++ utils/carriers.js
@@ -0,0 +1,26 @@
+const carriers = [
+  {
+    name: "CNA",
+    logo: "https://www.cna.com/web/wcm/connect/07f32ec0-1635-431e-88a9-e5495d256639/CNA+Logo+-+RedWhitebg.png?MOD=AJPERES",
+  },
+  { name: "Hagerty" },
+  { name: "Liberty" },
+  { name: "Mendota" },
+  { name: "Lincoln" },
+  { name: "Nationwide" },
+  { name: "Philadelphia" },
+  { name: "Progressive" },
+  { name: "SafeCo" },
+  { name: "The Hartford" },
+  { name: "Travelers" },
+  { name: "Zurich" },
+  { name: "Advantage Auto" },
+  { name: "The General" },
+  { name: "Burns and Wilcox" },
+  { name: "Western Pacific" },
+  { name: "Virtus Underwriters" },
+  { name: "Ryan and Turner" },
+  { name: "CRC Group" },
+];
+
+module.exports = carriers;
